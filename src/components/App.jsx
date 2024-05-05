f44b6223926d79913123c9cce12301e690698ba5
Aditya Raj <akaushik2@gitaam.in>
2024-05-05 12:08:39 +0530

    intro  stage 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./nav";
+import Intro from "./intro";
 
 
 
 function App(){
     return <div>
         <Header/>
+        <Intro/>
     </div>
 }
 
---
+++ src/components/intro.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../public/intro.css";
+function Intro(){
+    return <div className="Introduction">
+        <h3>Hello It's me</h3>
+        <h1>Aditya Raj Kaushik</h1>
+        <ul>
+            <li className="ab">And I’m a </li>
+            <li className="cd"> Full-Stack Developer</li>
+        </ul>
+        <p className="descryption">Fueled by a passion for tech innovation, I aim to utilize my skills in Computer Science and Engineering as a developer, contributing meaningfully to dynamic projects and driving positive change.</p>
+    </div>
+}
+
+export default Intro;
