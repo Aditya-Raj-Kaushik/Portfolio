a477a8b3330cdffdde9336fc37dc5c79ac916c45
Aditya Raj <akaushik2@gitaam.in>
2024-05-05 20:07:22 +0530

    done

--- src/components/intro.jsx
+++ src/components/intro.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import "../public/intro.css";
 function Intro(){
     return <div className="Introduction">
-        <h3>Hello It's me</h3>
+        <h3>Hello It’s me</h3>
         <h1>Aditya Raj Kaushik</h1>
         <ul>
-            <li className="ab">And I’m a </li>
+            <li className="ab">And I’m a</li>
             <li className="cd"> Full-Stack Developer</li>
         </ul>
         <p className="descryption">Fueled by a passion for tech innovation, I aim to utilize my skills in Computer Science and Engineering as a developer, contributing meaningfully to dynamic projects and driving positive change.</p>
+        <button className="button">Download CV</button>
     </div>
 }
 
